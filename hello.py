8358b477a6ae9f012044634ca61a284dca9a1ca8
Raul Andrade <raul.sandrade@hotmail.com>
2025-07-09 10:24:48 -0300

    utilizando variáveis de ambiente para definir a LANG do hello world

--- hello.py
+++ hello.py
@@ -1,4 +1,32 @@
 #!usr/bin/env python3
+"""Hello world multi linguas.
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente
 
-print("Olá Mundo !")
-input()
+Como usar: 
+Tenha a variável LANG devidamente configurada
+ex:
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+"""
+__version__ = "0.0.1" #dunder
+__author__ = "Raul Andrade"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5] # fatia para pegar apenas a linguagem, usa a en_US como default
+msg = "Hello, World !"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo !"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo !"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo !"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde !"
+
+print(msg)
