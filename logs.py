373b11455b1ce77d51d8e19fb3e038e5492235c6
Raul Andrade <raul.sandrade@hotmail.com>
2025-08-06 21:08:09 -0300

    criação do arquivo de logging

--- logs.py
+++ logs.py
@@ -2,19 +2,27 @@
 
 import logging
 import os
+from logging import handlers
 
 #TODO usar função
 #TODO usar lib loguru
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("logs.py",log_level )
-ch = logging.StreamHandler() #console / terminal/ stderr
-ch.setLevel(log_level)
+#ch = logging.StreamHandler() #console / terminal/ stderr
+#ch.setLevel(log_level)
+fh=handlers.RotatingFileHandler("meulog.log",#nome do arquivo
+                                 maxBytes=100,#tamanho do arquivo, geralmente é 10**6(1mb)
+                                   backupCount=10#quantidade de registros que ficam após chegar em 1mb)
+)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s  %(name)s  %(levelname)s'
     'l: %(lineno)d f: %(filename)s s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+#ch.setFormatter(fmt)
+#log.addHandler(ch)
+fh.setFormatter(fmt)
+log.addHandler(fh)
 
 """
 
