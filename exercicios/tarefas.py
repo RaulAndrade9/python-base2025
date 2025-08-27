84977773000992e464d6a6bd7135b826e02ebeee
Raul Andrade <raul.sandrade@hotmail.com>
2025-08-26 21:13:57 -0300

    Resoluçao de exercicios

---
+++ exercicios/tarefas.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python 3
+
+import logging
+import sys
+
+lista  = []
+
+lista.append({
+        "titulo":"estudar",
+        "descricao":"estudar um tema",
+        "status": "concluido"
+    })
+print(lista)
